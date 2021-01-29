2e2eb9c343b1adf801cc2a31a896ffcefd43224e
debian <p4wos8000@gmail.com>
2021-01-29 21:56:43 +0100

    added BlockTest in r08

---
+++ r08/block/src/BlockTest.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class BlockTest
+{
+    public static void main(String[] args)
+    {
+        new Block()
+            {
+                public void body() throws Exception
+                {
+                    Scanner in = new Scanner(new File("ququx"), "UTF-8");
+                    while (in.hasNext())
+                        System.out.println(in.next());
+                }
+            }
+            .toThread().start();
+    }
+}
+
+abstract class Block
+{
+    public abstract void body() throws Exception;
+
+    public Thread toThread()
+    {
+        return new Thread()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        body();
+                    }
+                    catch(Throwable t)
+                    {
+//                        Block.<RuntimeException>throwAs(t);
+                        throw t;
+                    }
+                }
+            };
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Throwable> void throwAs(Throwable e) throws T
+    {
+        throw (T) e;
+    }
+}
